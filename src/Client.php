bf441c6959fd6b89cd8a70f0f6ff1efdb4581c3e
Vijaya Chandran Mani <vijaycs85@gmail.com>
2020-12-20 01:52:46 +0000

    Changed theme to d.o

--- src/Client.php
+++ src/Client.php
@@ -23,27 +23,58 @@ class Client {
         $this->renderer = $renderer;
     }
 
-    protected function getData() {
-        $response = $this->httpClient->request('GET', self::API_ENDPOINT);
-        $content = json_decode($response->getBody());
-
+    protected function generate() {
+        $content = $this->getContent();
         foreach ($content as $delta => $item) {
             $date = DateTime::createFromFormat(DateTimeInterface::ATOM, $item->date);
             $item->version = $this->getVersion($item->title);
+            $item->thumbnail = $this->getThumbnail($item);
+            if (!$item->href) {
+                $item->href = '#';
+            }
             foreach (['description'] as $property)  {
                 $item->{$property} = $this->markdownConverter->convertToHtml($item->{$property});
             }
             if ($date) {
                 $result[$date->format('Y')][] = (array)$item;
             }
+
+
         }
         krsort($result);
         return $result;
     }
 
+    protected function getContent() {
+        $data_filename  = __DIR__ . '/../data/drupal-timeline.json';
+        return json_decode(file_get_contents($data_filename));
+
+        $response = $this->httpClient->request('GET', self::API_ENDPOINT);
+        if ($response->getStatusCode() !== 200) {
+            // Use the data available locally.
+            $response_body = file_get_contents($data_filename);
+        }
+        else {
+            // Save the latest file.
+            // @todo probably use HEAD and save with ETag instead of
+            // saving the same data multiple times.
+            $response_body = $response->getBody();
+            file_put_contents($data_filename, $response_body);
+        }
+        return json_decode($response_body);
+    }
+
     protected function getVersion($title) {
         preg_match('/^Drupal\s([0-9]\.[0-9]+)/s', $title, $matches);
         return isset($matches[1]) ? $matches[1] : NULL;
+    }
+    protected function getThumbnail($item) {
+        if ($item->version) {
+            if (file_exists(__DIR__ . "/../docs/assets/img/{$item->version}.png")) {
+                return "{$item->version}.png";
+            }
+        }
+        return 'default.png';
     }
 
     public function debugOn() {
@@ -53,6 +84,6 @@ class Client {
     }
 
     public function render() {
-        return $this->renderer->render($this->getData());
+        return $this->renderer->render($this->generate());
     }
 }
--- src/Renderer.php
+++ src/Renderer.php
@@ -8,8 +8,10 @@ use Twig\Environment;
 class Renderer {
 
     public function render($data) {
+        $cache = __DIR__ . '/../.cache';
+        $cache = FALSE;
         $loader = new FilesystemLoader(__DIR__ . '/../templates');
-        $twig = new Environment($loader, ['cache' => __DIR__ . '/../.cache']);
+        $twig = new Environment($loader, ['cache' => $cache]);
         return $twig->render('timeline.html.twig', ['data' => $data]);
     }
 }
